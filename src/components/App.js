e7a0adbbb81780f8e930a21a18a48cf1f0224eed
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-01-16 04:58:01 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [dog, setDog] = useState(null);
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setDog(data.message);
+            });
+    }, []);
+
+    if(!dog) return <p>Loading...</p>;
+
+    return <img src={dog} alt="A Random Dog" />;
+}
+
+export default App;
